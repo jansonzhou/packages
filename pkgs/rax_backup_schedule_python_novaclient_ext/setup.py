9f0101518639b993102bc63b3c1ce9027d4c2e12
Roma Koshel <roman@scalr.com>
2013-01-25 15:44:57 +0000

    rax_backup_schedule_python_novaclient_ext

--- pkgs/rax_backup_schedule_python_novaclient_ext/setup.py
+++ pkgs/rax_backup_schedule_python_novaclient_ext/setup.py
@@ -4,7 +4,7 @@ version = '0.1.2'
 setuptools.setup(
     name='rax_backup_schedule_python_novaclient_ext',
     version=version,
-    url='http://pypi.python.org/packages/source/p/python-swiftclient/python-swiftclient-%s.tar.gz' % version,
+    url='http://pypi.python.org/packages/source/r/rax_backup_schedule_python_novaclient_ext/rax_backup_schedule_python_novaclient_ext-%s.tar.gz' % version,
     license='Apache License (2.0)',
     author='Rackspace',
     author_email='rick.harris@rackspace.com'
