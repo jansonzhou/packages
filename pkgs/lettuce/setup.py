4f31ce3dcde0af30058fc0bbd2f07a451ed2128a
Roma Koshel <roman@scalr.com>
2013-01-25 17:48:23 +0000

    lettuce

---
+++ pkgs/lettuce/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+version = '0.2.12'
+setuptools.setup(
+    name='lettuce',
+    version=version,
+    url='http://pypi.python.org/packages/source/l/lettuce/lettuce-%s.tar.gz' % version,
+    license='GNU General Public License v3',
+    author='Gabriel Falcão',
+    author_email='gabriel@nacaolivre.org'
+)
