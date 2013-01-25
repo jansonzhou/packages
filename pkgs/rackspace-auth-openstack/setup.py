f6195edd576a5013fe083c074096f56ee30dfd56
Marat Komarov <marat@scalr.com>
2013-01-25 11:40:42 +0000

    rackspace-auth-openstack

--- pkgs/rackspace-auth-openstack/setup.py
+++ pkgs/rackspace-auth-openstack/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 version = '1.0'
 setuptools.setup(
-    name='rackspace-auth-client',
+    name='rackspace-auth-openstack',
     version=version,
     url='http://pypi.python.org/packages/source/r/rackspace-auth-openstack/rackspace-auth-openstack-%s.tar.gz' % version
 )
