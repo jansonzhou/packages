5ad53cac7d7e634a5d7cf72f575ca0799e8f55de
Marat Komarov <marat@scalr.com>
2013-01-28 12:57:44 +0000

    prettytable

--- pkgs/prettytable/setup.py
+++ pkgs/prettytable/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 version = '0.5'
 setuptools.setup(
-    name='python-prettytable',
+    name='prettytable',
     version=version,
     license='http://www.luke.maurits.id.au/software/bsdlicense.txt',
     url='http://pypi.python.org/packages/source/P/PrettyTable/prettytable-%s.tar.gz' % version,
