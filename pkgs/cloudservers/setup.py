bbe22880c30f88efd603bcb6694509c389ddc8ed
Roma Koshel <roman@scalr.com>
2013-01-28 12:01:02 +0000

    cloudservers

--- pkgs/cloudservers/setup.py
+++ pkgs/cloudservers/setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 version = '1.0'
 setuptools.setup(
-    name='boto',
+    name='python-cloudservers',
     version=version,
     license='BSD',
-    url='http://pypi.python.org/packages/source/p/python-cloudservers/python-%s.tar.gz' % version,
+    url='http://pypi.python.org/packages/source/p/python-cloudservers/python-cloudservers-%s.tar.gz' % version,
     author='Jacob Kaplan-Moss',
     author_email='jacob@jacobian.org'
 )
