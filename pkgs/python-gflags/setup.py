387728f187f7bdb6dc84518adde3e10447f83f88
Roma Koshel <roman@scalr.com>
2013-01-28 17:33:15 +0000

    python-gflags

--- pkgs/python-gflags/setup.py
+++ pkgs/python-gflags/setup.py
@@ -4,7 +4,7 @@ version = '2.0'
 setuptools.setup(
     name='python-gflags',
     version=version,
-    url='http://pypi.python.org/packages/source/p/python-gflags/python-gflags-%s.tar.gz' % version,
+    url='http://python-gflags.googlecode.com/files/python-gflags-%s.tar.gz' % version,
     license='BSD',
     author='Google Inc. and others',
     author_email='google-gflags@googlegroups.com'
