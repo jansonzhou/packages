7fd2cede62da97e5d708bd12123682f0ef5de798
Roma Koshel <roman@scalr.com>
2013-01-30 09:15:54 +0000

    sure

---
+++ pkgs/sure/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+version = '1.1.5'
+setuptools.setup(
+    name='sure',
+    version=version,
+    url='http://pypi.python.org/packages/source/s/sure/sure-%s.tar.gz' % version,
+    license='LGPL (3.0)',
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org'
+)
