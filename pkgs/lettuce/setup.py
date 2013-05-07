ea0673006fb8fe5e7eead1690d60ee546c8bcfdb
Marat Komarov <marat@scalr.com>
2013-05-07 11:11:44 +0300

    lettuce

--- pkgs/lettuce/setup.py
+++ pkgs/lettuce/setup.py
@@ -4,7 +4,6 @@ version = '0.2.14'
 setuptools.setup(
     name='lettuce',
     version=version,
-    #url='http://pypi.python.org/packages/source/l/lettuce/lettuce-%s.tar.gz' % version,
     url='https://github.com/Scalr/lettuce.git',
     license='GNU General Public License v3',
     author='Gabriel Falcao',
