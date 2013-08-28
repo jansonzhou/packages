0963ccaa14584bc131381191abb51e961a953da6
Marat Komarov <marat@scalr.com>
2013-08-28 15:48:00 +0300

    Synced CI instructions from production BuildBot

--- ci/master.py
+++ ci/master.py
@@ -1,9 +1,3 @@
-from buildbot.schedulers.basic import AnyBranchScheduler
-from buildbot.schedulers.triggerable import Triggerable
-from buildbot.changes.filter import ChangeFilter
-from buildbot.process.factory import BuildFactory
-
-
 project = __opts__['project']
 
 
@@ -24,7 +18,7 @@ c['builders'].append(dict(
 	name='{0} source'.format(project),
 	slavenames=['ubuntu1004'],
 	factory=BuildFactory(steps=
-		buildsteps.svn(__opts__) +
+		buildsteps.git(__opts__) +
 		buildsteps.source_dist_external(__opts__) +
 		buildsteps.trigger_packaging(__opts__, scheduler='packages packaging') +
 		buildsteps.to_repo(__opts__, types=['deb', 'rpm'], all_repos=True)
