a6b5ee332224cb0c1574323aa52f8ccf5bea76d0
Roma Koshel <roma.koshel@gmail.com>
2013-09-27 16:18:08 +0300

    pymongo-test

---
+++ pkgs/pymongo-test/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+version = '2.6.2'
+setuptools.setup(
+    name='pymongo-test',
+    version=version,
+    url='https://pypi.python.org/packages/source/p/pymongo/pymongo-%s.tar.gz' % version,
+    author='Mike Dirolf',
+    author_email='mongodb-user@googlegroups.com',
+)
