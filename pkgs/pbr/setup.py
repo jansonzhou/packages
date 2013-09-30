59960383d35aa7ad5da13ed9e1cf995dd2986151
Marat Komarov <marat@scalr.com>
2013-09-30 15:12:08 +0300

    pbr

---
+++ pkgs/pbr/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+version = '0.5.21'
+setuptools.setup(
+    name='pbr',
+    version=version,
+    url='https://pypi.python.org/packages/source/p/pbr/pbr-%s.tar.gz' % version,
+    license='Apache License (2.0)',
+    author='OpenStack, LLC.',
+    author_email='openstack-admins@lists.launchpad.net'
+)
