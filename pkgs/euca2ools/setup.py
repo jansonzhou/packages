44ca84116246028230ac36148741a2ab5fa7b2b6
Roma Koshel <roman@scalr.com>
2014-02-20 12:08:35 +0200

    euca2ools

--- pkgs/euca2ools/setup.py
+++ pkgs/euca2ools/setup.py
@@ -4,7 +4,7 @@ version = '3.0.2'
 setuptools.setup(
     name='euca2ools',
     version=version,
-    url='https://github.com/eucalyptus/euca2ools/archive/%s.tar.gz' % version
+    url='https://github.com/eucalyptus/euca2ools/archive/%s.tar.gz' % version,
     license='BSD (Simplified)',
     author='Eucalyptus Systems, Inc.',
     author_email='support@eucalyptus.com'
