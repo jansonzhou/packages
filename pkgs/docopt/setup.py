52ecf0b2622a70a1be7ca5092b93a395d8ccb7db
Marat Komarov <marat@scalr.com>
2014-06-23 18:34:12 +0300

    docopt

--- pkgs/docopt/setup.py
+++ pkgs/docopt/setup.py
@@ -4,7 +4,7 @@ version = '0.6.2'
 setuptools.setup(
     name='docopt',
     version=version,
-    url='https://pypi.python.org/packages/source/d/docopt/docopt-%s.tar.gz' % version
+    url='https://pypi.python.org/packages/source/d/docopt/docopt-%s.tar.gz' % version,
     license='MIT',
     author='Vladimir Keleshev',
     author_email='vladimir@keleshev.com'
