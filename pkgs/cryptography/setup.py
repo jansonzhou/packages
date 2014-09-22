24bb86cc202c8e9ff75a08c761647122f2aee357
Roma Koshel <roman@scalr.com>
2014-09-22 14:26:09 +0400

    cryptography

--- pkgs/cryptography/setup.py
+++ pkgs/cryptography/setup.py
@@ -6,7 +6,8 @@ setuptools.setup(
     version=version,
     url='https://pypi.python.org/packages/source/c/cryptography/cryptography-%s.tar.gz' % version,
     license='ASL',
-    author='The cryptography developers'
+    author='The cryptography developers',
+    platforms="ubuntu1204",
 )
 
 
