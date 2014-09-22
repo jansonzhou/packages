f615120661e6b4a8259de1bb45fd0173ae17be8a
Roma Koshel <roman@scalr.com>
2014-09-22 15:50:37 +0400

    pycparser

--- pkgs/pycparser/setup.py
+++ pkgs/pycparser/setup.py
@@ -4,7 +4,7 @@ version = '2.10'
 setuptools.setup(
     name='pycparser',
     version=version,
-    url='https://pypi.python.org/packages/source/p/pycparser/pycparser-%s.tar.gz' % version
+    url='https://pypi.python.org/packages/source/p/pycparser/pycparser-%s.tar.gz' % version,
     license='BSD',
     author='Eli Bendersky',
 )
