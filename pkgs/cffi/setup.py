027e73b1d526a5ba7df555b356dfc20a8e4db822
Roma Koshel <roman@scalr.com>
2014-09-22 15:39:19 +0400

    cffi

--- pkgs/cffi/setup.py
+++ pkgs/cffi/setup.py
@@ -7,5 +7,6 @@ setuptools.setup(
     url='https://pypi.python.org/packages/source/c/cffi/cffi-%s.tar.gz' % version,
     license='MIT',
     author='Armin Rigo, Maciej Fijalkowski',
+    platforms="ubuntu1204",
 )
 
